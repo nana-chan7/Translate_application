049e0c00ba5337d4db72d7fe60be3e9d50e5f537
frogBear_7 <{220098tt@yse-c.net}>
2024-01-30 10:19:47 +0900

    update

--- translateapp/views.py
+++ translateapp/views.py
@@ -7,27 +7,31 @@ import json
 from . import key # AWS認証用
 
 # AWS認証・リージョン
-aws_access_key_id = key.AWS_ACCESS_KEY_ID  # ID
-aws_secret_access_key = key.AWS_SECRET_ACCESS_KEY  # アクセスキー
-region = key.AWS_REGION  # リージョン
+# aws_access_key_id = key.AWS_ACCESS_KEY_ID  # ID
+# aws_secret_access_key = key.AWS_SECRET_ACCESS_KEY  # アクセスキー
+# region = key.AWS_REGION  # リージョン
 
-# AWS Translateを初期化
-translate = boto3.client('translate', 
-                        aws_access_key_id=aws_access_key_id, 
-                        aws_secret_access_key=aws_secret_access_key, 
-                        region_name=region)
-# Comprehendクライアントを初期化
-comprehend = boto3.client('comprehend', 
-                        aws_access_key_id=aws_access_key_id, 
-                        aws_secret_access_key=aws_secret_access_key, 
-                        region_name=region)
+
+# 認証情報 初期化関数
+def aws_initialization(aws_id, aws_key, region):
+    # AWS Translateを初期化
+    translate = boto3.client('translate', 
+                            aws_access_key_id=aws_id, 
+                            aws_secret_access_key=aws_key, 
+                            region_name=region)
+    # Comprehendクライアントを初期化
+    comprehend = boto3.client('comprehend', 
+                            aws_access_key_id=aws_id, 
+                            aws_secret_access_key=aws_key, 
+                            region_name=region)
+    
+    return translate, comprehend
 
 # トップページ
 @app.route('/')
 def index():
     return render_template('translateapp/index.html')
 
-# 翻訳ページ
 @app.post('/main')
 @app.get('/main')
 def main():
@@ -35,12 +39,21 @@ def main():
         return render_template('translateapp/main.html')
     
     if request.method == 'POST':
-        # ソース言語 (日本語)
-        source_lang = "ja"
+        
+        # 認証情報を取得
+        res_id = request.form['aws_access_key_id']
+        res_key = request.form['aws_secret_access_key']
+        res_region = request.form['region']
         
         # 翻訳したい日本語テキストを取得
         input_text = request.form['text']
         target_lang = request.form['target_lang']
+        
+        # AWS認証とクライアントの初期化
+        translate, _ = aws_initialization(res_id, res_key, res_region)  
+        
+        # ソース言語 (日本語)
+        source_lang = "ja"
         
         # テキストを指定された言語に翻訳
         translate_result = translate.translate_text(Text=input_text, SourceLanguageCode=source_lang, TargetLanguageCode=target_lang)
